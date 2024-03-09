6170434f0a79f62d07480dfea6538dcd8ef58b89
Ly Dang <lydang26993@gmail.com>
2024-03-09 11:57:50 -0700

    update changes DB name

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -3,7 +3,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340db;
+    OWNER TO cse340la;
 
 -- Table structure for table 'classification'
 CREATE TABLE public. classification (
