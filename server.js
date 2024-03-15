90182cf7dd521d23e9e1fd882334529fe8d74625
Ly Dang <lydang26993@gmail.com>
2024-03-14 18:03:12 -0600

    added inventoryRoute to fix this error: app.use("/inv", inventoryRoute) ReferenceError: inventoryRoute is not defined

--- server.js
+++ server.js
@@ -5,19 +5,20 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root
 
 
 /* ***********************
@@ -27,27 +28,27 @@ app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 
 // // Index route
 // app.get("/", function(req, res){
 //   res.render("index", {title: "Home"})
 // })
 // Index route//Alter the "Index Route" using the entire M-V-C approach
-app.get("/", baseController.buildHome)
+app.get("/", baseController.buildHome);
 // Inventory routes
-app.use("/inv", inventoryRoute)
+app.use("/inv", inventoryRoute);
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
-})
+});
