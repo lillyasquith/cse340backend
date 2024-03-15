6ea3fd0ead59f89e40d77a99dd31fcb6e1ef6e5e
Ly Dang <lydang26993@gmail.com>
2024-03-14 18:01:22 -0600

    finish Classification View

--- utilities/index.js
+++ utilities/index.js
@@ -12,13 +12,13 @@ Util.getNav = async function (req, res, next) {
     data.rows.forEach((row) => {
         list += "<li>"
         list +=
-      '<a href="/inv/type/' +
-      row.classification_id +
-      '"title" = See our inventory of' +
-      row.classification_name +
-      'vehicles">' +
-      row.classification_name +
-      "</a>"
+          '<a href="/inv/type/' +
+          row.classification_id +
+          '" title="See our inventory of ' +
+          row.classification_name +
+          ' vehicles">' +
+          row.classification_name +
+          "</a>"
     list += "</li>"
     })
     list += "</ul>"
