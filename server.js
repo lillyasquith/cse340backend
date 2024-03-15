8c0026afcc18e182bfaf439536e0ac3749a27c91
Ly Dang <lydang26993@gmail.com>
2024-03-14 22:47:22 -0600

    update changes

--- server.js
+++ server.js
@@ -40,6 +40,11 @@ app.get("/", baseController.buildHome);
 // Inventory routes
 app.use("/inv", inventoryRoute);
 
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
 
 /* ***********************
 * Express Error Handler
@@ -51,7 +56,7 @@ app.use(async (err, req, res, next) => {
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message: err.message,
-    nav
+    nav,
   })
 });
 
